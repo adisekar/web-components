a2469d8a39ee26f28e7bc3a4e20d5d629a3e3cb0
adisekar <adithya.kiran.sekar@gmail.com>
2022-08-03 22:05:50 -0400

    Add tooltip component

---
+++ web-components-stencil/src/components/tooltip/tooltip.tsx
@@ -0,0 +1,26 @@
+import { Component, Prop, State, h } from '@stencil/core';
+
+@Component({
+  tag: 'uc-tooltip',
+  styleUrl: 'tooltip.css',
+  shadow: true
+})
+export class Tooltip {
+  @State() tooltipShown = false;
+
+  @Prop({ reflect: true }) title: string;
+
+
+  showToolTip() {
+    this.tooltipShown = !this.tooltipShown;
+  }
+
+  render() {
+    return (
+      <div>
+        <slot></slot>  <button onClick={this.showToolTip.bind(this)}> (?)</button>
+        <div class={`tooltip ${this.tooltipShown ? "show-tooltip" : "hide-tooltip"}`}>{this.title} </div>
+      </div>
+    );
+  }
+}
